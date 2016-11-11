86a65a3635644b13bda713fcdcb9080b9bb4e962
Kevin Murray <spam@kdmurray.id.au>
2016-11-11 18:18:40 +1100

    Add khmer util and change b2f algorithm

--- bam2pairedfq/bam2pairedfq.py
+++ bam2pairedfq/bam2pairedfq.py
@@ -1,54 +1,47 @@
 #!/usr/bin/env python3
-from collections import namedtuple
 import pysam
+import screed
+import khmer_utils as ku
 from sys import argv, stderr, stdout
 
 
-Record = namedtuple('Record', ['name', 'seq', 'pair', 'rname', 'pos'])
-
-
-def printrec(rec, file=None):
-    print(">{}/{}\n{}".format(rec.name, rec.pair, rec.seq), file=file)
-
-
-def printpair(a, b, file=None):
-    if a.pair == '1':
-        printrec(a, file)
-        printrec(b, file)
+def printread(read, file=None):
+    if read.quality:
+        print("@", read.name, ' ', read.description, sep='', file=file)
+        print(read.sequence, file=file)
+        print("+", file=file)
+        print(read.quality, file=file)
     else:
-        printrec(b, file)
-        printrec(a, file)
+        print(">", read.name, ' ', read.description, sep='', file=file)
+        print(read.sequence, file=file)
 
 
-def read2rec(read):
-    name = read.query_name
-    readno = '1' if read.is_read1 else '2'
-    return Record(name, read.seq, readno, read.next_reference_name,
-                  read.next_reference_start)
+def notag(readid):
+    readid, _, _ = readid.partition(' ')
+    if readid[-2] == '/' and readid[-1] in "12":
+        return readid[:-2]
+    return readid
 
 
-def process_file(bamfiles, regions, outstream=stdout):
-    tofetch = {}
-    for bamfile in bamfiles:
-        samf = pysam.AlignmentFile(bamfile, 'rb')
-        for region in regions:
-            for read in samf.fetch(region):
-                if read.is_unmapped or read.is_secondary:
-                    continue
-                this = read2rec(read)
-                if not read.is_paired:
-                    printrec(this)
-                if this.name in tofetch:
-                    other = tofetch.pop(this.name)
-                    printpair(this, other, file=outstream)
-                else:
-                    tofetch[this.name] = this
-
-        for name, have in tofetch.items():
-            for read in samf.fetch(have.rname, have.pos - 10, have.pos + 10):
-                need = read2rec(read)
-                if need.name == have.name and need.pair != have.pair:
-                    printpair(have, need, file=outstream)
+def process_file(bamfile, regions, readfile, outstream=stdout):
+    samf = pysam.AlignmentFile(bamfile, 'rb')
+    to_extract = set()
+    for region in regions:
+        for read in samf.fetch(region):
+            name = read.query_name
+            if not read.is_secondary and not read.is_supplementary:
+                to_extract.add(name)
+    print("Collected", len(to_extract), "read names to extract", file=stderr)
+    with screed.open(readfile) as fq:
+        for n, is_pair, read1, read2 in ku.broken_paired_reader(fq):
+            if is_pair:
+                if notag(read1.name) in to_extract:
+                    ku.write_record_pair(read1, read2, stdout)
+            else:
+                read = read1 if read1 else read2
+                if notag(read.name) in to_extract:
+                    ku.write_record(read, stdout)
+    print("Done", file=stderr)
 
 
 def cliparse():
@@ -58,11 +51,13 @@ def cliparse():
     )
     psr.add_argument('-r', '--region', action='append',
                      help='Region to extract')
-    psr.add_argument('bam', nargs='+',
+    psr.add_argument('fastq',
+                     help='Interleaved fastq of reads')
+    psr.add_argument('bam',
                      help='Alignment file')
     return psr.parse_args()
 
 
 if __name__ == "__main__":
     args = cliparse()
-    process_file(args.bam, args.region)
+    process_file(args.bam, args.region, args.fastq)
